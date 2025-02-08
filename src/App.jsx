8751c15cac43152d62080fe00a8bf871a8befa50
mahmud2237 <smahmud2237@gmail.com>
2025-02-08 13:41:30 +0600

    My Todo list code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
+import "./App.css";
 
-import './App.css'
 
 function App() {
-
-  const todoTitle = 'My Todo List';
+  const todoTitle = "My Todo List";
   const date = new Date();
   const dateName = date.getDate();
   const monthName = date.getMonth();
   const currentYear = date.getFullYear();
 
-  const headingStyle ={
-    backgroundColor: 'purple',
-    color: 'white',
-    fontSize: '4rem',
-    textAlign: 'center',
-    padding: '15px'
-  }
+  const Card = () => {
+    return (
+      <div className="card">
+        <h2 style={{ color: "red", fontSize: "3rem" }}>{todoTitle}</h2>
+        <p className="app">
+          Date: {dateName}/{monthName}/{currentYear}
+        </p>
+        <ul>
+          <li>Learn React</li>
+          <li>Learn Node</li>
+          <li>Learn Express</li>
+          <li>Learn MongoDB</li>
+        </ul>
+      </div>
+    );
+  };
+
+  const headingStyle = {
+    backgroundColor: "purple",
+    color: "white",
+    fontSize: "4rem",
+    textAlign: "center",
+    padding: "15px",
+  };
 
   return (
     <>
-  
       <h1 style={headingStyle}>Todo App</h1>
-      <h2 style={{ color:'red', fontSize:'3rem' }}>{todoTitle}</h2>
-      <p className='app'>Date: {dateName}/{monthName}/{currentYear}</p>
-      <ul>
-        <li>Learn React</li>
-        <li>Learn Node</li>
-        <li>Learn Express</li>
-        <li>Learn MongoDB</li>
-      </ul>
-      
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
